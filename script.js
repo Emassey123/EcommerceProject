565dec92470eb26558c41b2dc0a696414263138e
Emassey123 <94918775+Emassey123@users.noreply.github.com>
2022-01-25 15:51:00 -0500

    updated a few things in the html and css

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
 const hamburger = document.getElementById('hamburger;')
 const navUL = document.getElementById('nav-ul');
 
-hamburger.addEventListener('click', () => {
-    navUL.classList.toggle('show')
-});
